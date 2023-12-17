923523d98e4b67072256e613376e5786b0e80f0b
alinetixsol <ali_muhammad_intern@netixsol.com>
2023-12-17 08:12:03 -1200

    First Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Filters from "./components/filters/Filters";
+import Header from "./components/header/Header";
+import List from "./components/list/List";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <List />
+    </div>
+  );
+}
+
+export default App;
